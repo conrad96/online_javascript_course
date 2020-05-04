aae7e00146cbcb34ce243b6a19ac97eb148f89ed
conrad96 <conrad.mugisha@gmail.com>
2020-05-05 00:37:28 +0300

    acess object methods

--- lecture_1.js
+++ lecture_1.js
@@ -3,4 +3,17 @@ const item = 42
 
 const explicit  = String(item)
 
-console.log( typeof explicit)
+//console.log( typeof explicit)
+
+//object
+
+const x = new Object();
+
+x.firstName = 'Conrad';
+x.lastName = 'Mugisha';
+x.sayHi = function() {
+    console.log('Hi');
+}
+
+//console.log(x);
+console.log( x.sayHi() );
