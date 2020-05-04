441352cc725f61aae008d6d63f619169cf873c21
conrad96 <conrad.mugisha@gmail.com>
2020-05-05 01:11:37 +0300

    deepcopy an object

--- lecture_1.js
+++ lecture_1.js
@@ -14,7 +14,31 @@ x.lastName = 'Mugisha';
 x.sayHi = function() {
     console.log('Hi');
 }
+x.child = {
+    "one": "value 1",
+    "two": "value 2"
+}
 
 //console.log(x);
 //console.log( x.sayHi() );
-console.log(x.firstName);
+//console.log(x.firstName);
+
+//deepCopy object
+function deepCopy(obj) {
+    const keys = Object.keys(obj)
+    const newObject = {}
+
+    for(let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        if( typeof obj[ key] === 'object') {
+            newObject[key] = deepCopy(obj[key])
+        } else {
+            newObject[key] = obj[key]
+        }
+    }
+
+    return newObject
+}
+
+const obj = deepCopy(x)
+console.log(obj)
